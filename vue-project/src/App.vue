15b12b5a0c01fd0cc15ce0a2da4eac69a2f237f8
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2024-01-11 18:18:24 +0300

    Реализация добавления заметки + базовая стилизация (11.01.24)

---
+++ vue-project/src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import HelloWorld from "./components/Input.vue";
+import Texts from "./components/Texts.vue";
+</script>
+
+<template>
+  <input
+    class="mainInput"
+    type="text"
+    placeholder="Напишите ваш текст"
+    @keyup.enter="addText"
+    v-model="writeText"
+  />
+
+  <Texts :texts="texts" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      texts: [
+        {
+          id: 1,
+          text: "Элемент 1 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente unde, delectus est voluptas ducimus architecto laborum. Deserunt et eligendi consequuntur, quasi labore cupiditate, id enim veritatis aperiam, quo eos aspernatur.",
+        },
+        { id: 2, text: "Элемент 2" },
+        { id: 3, text: "Элемент 3" },
+      ],
+
+      writeText: "",
+    };
+  },
+
+  methods: {
+    addText() {
+      console.log("Клик на Enter");
+      this.texts.push({ id: Date.now(), text: this.writeText });
+    },
+  },
+});
+</script>
---
+++ vue-project/src/components/Texts.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 style="margin-top: 30px">Список текстов</h1>
+
+  <ul class="list">
+    <li class="list-item" v-for="item in texts" :key="item.id">
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  methods: {},
+  props: {
+    texts: {
+      type: Object,
+    },
+  },
+});
+</script>
