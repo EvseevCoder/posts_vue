e25d53b278a10a115dee2490e74973cd223fd6ef
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2024-01-14 18:44:43 +0300

    Реализация удаления (14.01.24)

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,6 +5,14 @@ import Texts from "./components/Texts.vue";
 
 <template>
   <input
+    type="text"
+    placeholder="Введите ваше имя"
+    v-model="nextName"
+    @keyup.enter="createName"
+  />
+  <p>{{ Name }}</p>
+  <input
+    v-if="edits.editMode == false"
     class="mainInput"
     type="text"
     placeholder="Напишите ваш текст"
@@ -12,7 +20,16 @@ import Texts from "./components/Texts.vue";
     v-model="writeText"
   />
 
-  <Texts :texts="texts" />
+  <!-- Поле для редактирования текста -->
+  <input
+    v-if="edits.editMode == true"
+    class="mainInput"
+    type="text"
+    v-model="edits.editText"
+    @keyup.enter="createEdit"
+  />
+
+  <Texts :texts="texts" @delite="delite" @edit="edit" />
 </template>
 
 <script lang="ts">
@@ -24,20 +41,60 @@ export default defineComponent({
       texts: [
         {
           id: 1,
+          author: "User",
           text: "Элемент 1 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente unde, delectus est voluptas ducimus architecto laborum. Deserunt et eligendi consequuntur, quasi labore cupiditate, id enim veritatis aperiam, quo eos aspernatur.",
         },
-        { id: 2, text: "Элемент 2" },
-        { id: 3, text: "Элемент 3" },
+        { id: 2, text: "Элемент 2", author: "User" },
+        { id: 3, text: "Элемент 3", author: "User" },
       ],
 
       writeText: "",
+      Name: "User",
+      nextName: "",
+      edits: {
+        editMode: false,
+        editText: "",
+        editId: 0,
+      },
     };
   },
 
   methods: {
     addText() {
-      console.log("Клик на Enter");
-      this.texts.push({ id: Date.now(), text: this.writeText });
+      if (this.writeText) {
+        this.texts.push({
+          id: Date.now(),
+          text: this.writeText,
+          author: this.Name,
+        });
+        this.writeText = "";
+      }
+    },
+    delite(id: number) {
+      this.texts = this.texts.filter((item) => item.id != id);
+    },
+    edit(id: number) {
+      this.edits.editMode = true;
+      for (const text of this.texts) {
+        if (text.id == id) {
+          this.edits.editId = id;
+          this.edits.editText = text.text;
+        }
+      }
+    },
+    createEdit() {
+      this.texts.forEach((text) => {
+        if (text.id == this.edits.editId) {
+          text.text = this.edits.editText;
+          this.edits.editText = "";
+          this.edits.editId = 0;
+          this.edits.editMode = false;
+        }
+      });
+    },
+    createName() {
+      this.Name = this.nextName;
+      this.nextName = "";
     },
   },
 });
--- vue-project/src/components/Texts.vue
+++ vue-project/src/components/Texts.vue
@@ -3,7 +3,9 @@
 
   <ul class="list">
     <li class="list-item" v-for="item in texts" :key="item.id">
-      {{ item.text }}
+      {{ item.author }} : {{ item.text }}
+      <button @click="delite(item.id)">Удалить</button>
+      <button @click="edit(item.id)">Редактировать</button>
     </li>
   </ul>
 </template>
@@ -12,11 +14,27 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  methods: {},
+  data() {
+    return {
+      item: "",
+    };
+  },
+  methods: {
+    delite(id: number) {
+      this.$emit("delite", id);
+    },
+    edit(id: number) {
+      this.$emit("edit", id);
+    },
+  },
   props: {
     texts: {
       type: Object,
     },
+  },
+  emits: {
+    delite: (id: number) => Number.isInteger(id),
+    edit: (id: number) => Number.isInteger(id),
   },
 });
 </script>
